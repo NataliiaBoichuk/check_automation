63136c95873a0bdbb9fb2bcd27b7cf1540adbe5c
NataliiaBoichuk <natalia.boichuk@qatestlab.eu>
2021-09-03 17:15:49 +0300

    add path to local driver

--- conftest.py
+++ conftest.py
@@ -52,7 +52,7 @@ def browser(request):
     if browser_name == 'firefox':
         driver = webdriver.Firefox(executable_path=GeckoDriverManager(version='91.0.2').install())
     else:
-        driver = webdriver.Chrome(executable_path='/usr/local/bin')
+        driver = webdriver.Chrome(executable_path='/usr/bin')
 
     browser = EventFiringWebDriver(driver, MyListener())
 
